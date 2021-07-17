3bec94ae03bc1f8c6db8670347867751066dcccf
usilwal <usilwal@hotmail.com>
2021-07-17 18:45:59 -0400

    implemented vigenere encryption algorithm

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-const alphaNum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const vigenereTable = create_vigenere_table();
 const yourMessage = document.getElementById('yourMessage');
+const keywordV = document.getElementById('keywordV');
 let lettersNum = document.getElementById('lettersNum');
 let numbersNum = document.getElementById('numbersNum');
-const encrypt = document.getElementById('encrypt');
-const decrypt = document.getElementById('decrypt');
+const encryptC = document.getElementById('encryptC');
+const encryptV = document.getElementById('encryptV');
+const decryptC = document.getElementById('decryptC');
 const modifiedMessage = document.getElementById('modifiedMessage');
 
-encrypt.addEventListener('click', function(e) {
+encryptC.addEventListener('click', function(e) {
     modifiedMessage.value = caesar(yourMessage.value, lettersNum.value, numbersNum.value);
 });
 
-decrypt.addEventListener('click', function(e) {
+decryptC.addEventListener('click', function(e) {
     modifiedMessage.value = caesar_decrypt(yourMessage.value, lettersNum.value, numbersNum.value);
+});
+
+encryptV.addEventListener('click', function(e) {
+    modifiedMessage.value = vigenere_encrypt(yourMessage.value.toUpperCase(), keywordV.value.toUpperCase());
 });
 
 function caesar(message, lettersDown, numbersDown) {
@@ -38,4 +44,24 @@ function caesar(message, lettersDown, numbersDown) {
 
 function caesar_decrypt(message, lettersUp, numbersUp) {
     return caesar(message, 26 - lettersUp, 10 - numbersUp)
+}
+
+function create_vigenere_table() {
+    let table = []
+    for (let i = 0; i < 26; i++) {
+        modAlphabet = '';
+        for (let j = 0; j < 26; j++) {
+            modAlphabet += caesar(alphabet[j], i, i)
+        }
+        table.push(modAlphabet)
+    }
+    return table
+}
+
+function vigenere_encrypt(message, key) {
+    encryptedMessage = '';
+    for (let i = 0; i < message.length; i++) {
+        encryptedMessage += caesar(message[i], key[i % key.length].charCodeAt() - 65, 0)
+    }
+    return encryptedMessage;
 }
