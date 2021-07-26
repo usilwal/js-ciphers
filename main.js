16f7601cbc0c31a021348e17f7ef5fa54e27007e
usilwal <usilwal@hotmail.com>
2021-07-25 20:55:19 -0400

    added button to randomize inputs for ciphers

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const lettersNum = document.getElementById('lettersNum');
 const numbersNum = document.getElementById('numbersNum');
 const encrypt = document.getElementById('encrypt');
 const decrypt = document.getElementById('decrypt');
+const randomize = document.getElementById('randomize');
 const modifiedMessage = document.getElementById('modifiedMessage');
 
 encrypt.addEventListener('click', function(e) {
@@ -16,8 +17,49 @@ decrypt.addEventListener('click', function(e) {
     run_cipher('d');
 });
 
+randomize.addEventListener('click', function(e) {
+    value_randomizer();
+});
+
+function get_cipher_type() {
+    return document.querySelector('input[name="cipher"]:checked').value
+}
+
+function randomize_key_helper(keyLength, asciiRange) {
+    //randomizing for loop for vigenere and monoalphabetic keys
+}
+
+function value_randomizer() {
+    let keyLength = 0;
+    let asciiRange = [0, 0]
+    switch(get_cipher_type()) {
+        case 'caesar':
+            lettersNum.value = Math.floor(Math.random() * 25);
+            numbersNum.value = Math.floor(Math.random() * 9);
+            break;
+        case 'vigenere':
+            keyLength = yourMessage.value.length;
+            asciiRange = [65, 90]
+            break;
+        case 'monoalphabetic':
+            keyLength = 26;
+            asciiRange = [33, 126]
+            break;
+        default:
+            break;
+    }
+    if (keyLength != 0) {
+        key.value = ''
+        const chars = new Set();
+        while(chars.size < keyLength) {
+            chars.add(String.fromCharCode(asciiRange[0] + Math.floor(Math.random() * (asciiRange[1] - asciiRange[0]))))
+        }
+        chars.forEach((char) => key.value += char)
+    }
+}
+
 function run_cipher(mode) {
-    switch(document.querySelector('input[name="cipher"]:checked').value) {
+    switch(get_cipher_type()) {
         case 'caesar':
             (mode === 'e') ? 
             modifiedMessage.value = caesar(yourMessage.value, lettersNum.value, numbersNum.value) : modifiedMessage.value = caesar_decrypt(yourMessage.value, lettersNum.value, numbersNum.value);
@@ -92,6 +134,7 @@ function create_monoalphabetic_table(key) {
     //store the differences in ascii value between alphabet and key 
     let table = [];
     for (let i = 0; i < 26; i++) {
+        console.log(key[i].charCodeAt() - alphabet[i].charCodeAt())
         table.push(key[i].charCodeAt() - alphabet[i].charCodeAt());
     }
     return table;
